d8d4bdbdd974ab5384f36d1e56593c11f103db6d
nariclee7 <2095055676@qq.com>
2025-04-27 16:33:56 +0800

    first commit

---
+++ Singleton/src/main/java/org/example/SingleTon.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class SingleTon {
+  /**
+   * 单例模式：有且仅有一个元素的集合
+   */
+}
---
+++ Singleton/src/main/java/org/example/Sun.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Sun {
+  private static Sun sun = new Sun();
+
+  private Sun() {
+    // 构造方法私有化
+
+  }
+}
